ae749b7b02b1d8b5a778619611e58c02cb3bc556
Huzmaan <princehuzmaanpasta@gmail.com>
2021-10-08 00:12:44 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import Main from './Components/Main'
+
+function App() {
+  return (  
+   <>
+   <Header></Header>
+   <Main></Main>
+   <Footer></Footer>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+ const Header = () => {
+    return (
+        <div className="header">
+            <div className="home-css">
+            <p>Home</p>
+            </div>
+            <p className="text-css">Link1</p>
+            <p className="text-css">Link2</p>
+            <p className="text-css">Link3</p>
+            <p className="text-css">Link4</p>
+        </div>
+    )
+}
+
+
+export default Header;
