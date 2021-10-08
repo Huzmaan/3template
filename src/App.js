0ce194dcdeb02dd68d607df9520e5a9bc3cc6bd1
Huzmaan <princehuzmaanpasta@gmail.com>
2021-10-09 00:04:26 +0500

    css-update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './Components/Footer'
 import Header from './Components/Header'
 import Main from './Components/Main'
 
+
 function App() {
   return (  
    <>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faAnchor}  from "@fortawesome/free-solid-svg-icons";
 
- const Main = () => {
-    return (
-        <div>
-            <h1>Main</h1>
+const Main = () => {
+  return (
+    <>
+      <div className="Main-css">
+        <h1>START PAGE</h1>
+        <h6>Template by Huzmaan</h6>
+        <button className="btn-css">GET STARTED</button>
+      </div>
+      <div className="lorem-css">
+          <div className="textcss">
+        <h3>Lorem Ipsum</h3>
+        {/* <i class="fas fa-anchor"></i>{" "} */}
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+          nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+        </p>
         </div>
-    )
-}
-
-
+        <FontAwesomeIcon  className="icon-css" icon={faAnchor}/>
+      </div>
+    </>
+  );
+};
 
 export default Main;
