02550aa919ba657e2ba6e2484758b901dbe2a9e7
Huzmaan <princehuzmaanpasta@gmail.com>
2021-10-10 07:07:04 +0500

    navbar-working

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,27 +1,40 @@
-import React from 'react'
+import React from "react";
 
- const Header = () => {
-    return (
-        <>
-        <div className="header-css">
-        <div className="header">
-            <div className="home-css">
-            <h4>Home</h4>
-            </div>
-            <h4 className="text-css">Link1</h4>
-            <h4 className="text-css">Link2</h4>
-            <h4 className="text-css">Link3</h4>
-            <h4 className="text-css">Link4</h4>
+const Header = () => {
+  function navbar() {
+    var x = document.getElementsByClassName("topnav");
+    console.log(x.className)
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  return (
+    <>
+      <div className="header-css">
+        <div className="topnav" id="myTopnav">
+          <a href="#home" class="active">
+            Home
+          </a>
+          <a href="#Link1">Link1</a>
+          <a href="#Link2">Link2</a>
+          <a href="#Link3">Link3</a>
+          <a href="#Link4">Link4</a>
+         <button onClick={navbar}   >  
+          <a href="javascript:void(0);"  className="icon">
+            &#9776;
+          </a>
+            </button>
         </div>
         <div className="Main-css">
-        <h1 className="start-css">START PAGE</h1>
-        <h6>Template by Huzmaan</h6>
-        <button className="btn-css">GET STARTED</button>
+          <h1 className="start-css">START PAGE</h1>
+          <h6>Template by Huzmaan</h6>
+          <button className="btn-css">GET STARTED</button>
+        </div>
       </div>
-      </div>
-      </>
-    )
-}
-
+    </>
+  );
+};
 
 export default Header;
