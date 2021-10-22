a6f2f7b7ee88b878737b0d5545809aad54743232
Huzmaan <princehuzmaanpasta@gmail.com>
2021-10-23 00:16:02 +0500

    'cssWork'

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <nav className="Navbar">
         <h3 className="Home">Home</h3>
-        <ul className= {isMobile ? "nav-links-mobile":"Navlink"}
+        <ul  className= {isMobile ? "nav-links-mobile":"Navlink"}
             onClick={()=>setIsMobile(false)}>
        <li className="li-css"> <Link to="/Link1" className="Link1"> </Link> Link1</li>
        <li className="li-css"> <Link to="/Link2" className="Link2"></Link> Link2</li>
@@ -16,7 +16,7 @@ const Navbar = () => {
         </ul>
         <button className="mobile-menu-icon"
         onClick={()=>setIsMobile(!isMobile)}>
-        {isMobile? (<i className="fa fa-times"></i>):(<i className="fa fa-bars"></i>)}
+        {isMobile? (<i style={{color:"black"}} className="fa fa-bars"></i>):(<i   className="fa fa-bars"></i>)}
         </button>
         </nav>
         
